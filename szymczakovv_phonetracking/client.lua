e207878e33a00fbbd00db05984d0f5e793a2584d
Marcel <67207947+szymczakovv@users.noreply.github.com>
2021-10-16 20:23:02 +0200

    Update client.lua

--- szymczakovv_phonetracking/client.lua
+++ szymczakovv_phonetracking/client.lua
@@ -1,3 +1,12 @@
+ESX = nil
+
+Citizen.CreateThread(function()
+  while ESX == nil do
+    TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+    Citizen.Wait(0)
+  end
+end)
+
 RegisterNetEvent('szymczakovv:phoneTrackedSuccesfully')
 AddEventHandler('szymczakovv:phoneTrackedSuccesfully', function(data)
   local getted = {
